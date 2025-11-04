949b5bc4a42bb8a7f5c33e265fb6541345e36649
iziz49240-netizen <iziz49240@gmail.com>
2025-11-04 22:39:58 +0100

    Update index.ts

--- discordbotroblox/server/index.ts
+++ discordbotroblox/server/index.ts
@@ -1,6 +1,7 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
-import { setupVite, serveStatic, log } from "./vite";
+import { setupVite } from "./vite";
+
 
 const app = express();
 
